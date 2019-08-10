c0641cf87df38790bdcb9ef6aa0593f859677910
radhakrishnan <radha.ta2008@gmail.com>
2019-08-10 22:28:19 +0530

    new

---
+++ ms_bookingsrvc/src/main/java/com/zygotecorp/app/MsBookingsrvcApplication.java
@@ -0,0 +1,13 @@
+package com.zygotecorp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookingsrvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookingsrvcApplication.class, args);
+	}
+
+}
