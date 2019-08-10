8a61c19ddc7306c4851c6d433526efa37a98cac5
radhakrishnan <radha.ta2008@gmail.com>
2019-08-10 22:34:00 +0530

    scratch

---
+++ ms_cartserver/src/main/java/com/zygotecorp/app/CartEurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.zygotecorp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CartEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartEurekaServerApplication.class, args);
+	}
+
+}
---
+++ ms_notifysrvc/src/main/java/com/zygotecorp/app/MsNotifysrvcApplication.java
@@ -0,0 +1,13 @@
+package com.zygotecorp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotifysrvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotifysrvcApplication.class, args);
+	}
+
+}
