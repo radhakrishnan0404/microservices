0b3a95eea67f5a9cfa3f3550ea1d896fe2f5b101
radhakrishnan <radha.ta2008@gmail.com>
2019-09-01 20:15:57 +0530

    controller

--- config-server/src/main/java/com/zygotecorp/app/ConfigServerApplication.java
+++ config-server/src/main/java/com/zygotecorp/app/ConfigServerApplication.java
@@ -1,15 +1,32 @@
 package com.zygotecorp.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableConfigServer
+
 @SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+	
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+	 
+	    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+	    private String msg;
+	 
+	    @RequestMapping("/msg")
+	    String getMsg() {
+	        return this.msg;
+	    }
 	}
 
 }
