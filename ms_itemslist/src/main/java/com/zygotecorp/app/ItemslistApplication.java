a5d855aa99eb3f05543cc178f6d3f8ab883022d7
radhakrishnan <radha.ta2008@gmail.com>
2019-09-14 01:12:41 +0530

    new chg

--- ms_itemslist/src/main/java/com/zygotecorp/app/ItemslistApplication.java
+++ ms_itemslist/src/main/java/com/zygotecorp/app/ItemslistApplication.java
@@ -3,8 +3,10 @@ package com.zygotecorp.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ItemslistApplication {
 
 	public static void main(String[] args) {
--- ms_queuer/src/main/java/com/zygotecorp/app/MsQueuerApplication.java
+++ ms_queuer/src/main/java/com/zygotecorp/app/MsQueuerApplication.java
@@ -2,14 +2,12 @@ package com.zygotecorp.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MsPaymentsApplication {
+public class MsQueuerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsPaymentsApplication.class, args);
+		SpringApplication.run(MsQueuerApplication.class, args);
 	}
 
 }
