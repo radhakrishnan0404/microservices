3e7f1ec8e2ceea54f6c08977a783ba2011bde54b
radhakrishnan <radha.ta2008@gmail.com>
2019-09-15 18:34:15 +0530

    admin update

---
+++ ms_adminclient/src/main/java/com/zygotecorp/app/MsAdminclientApplication.java
@@ -0,0 +1,13 @@
+package com.zygotecorp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminclientApplication.class, args);
+	}
+
+}
