5b75aab657c5c262a7d9474905bc6aae61428f40
radhakrishnan <radha.ta2008@gmail.com>
2019-10-05 12:20:48 +0530

    new daos

---
+++ MongoDBDAOLayer/src/main/java/com/zygotecorp/app/MongoDbdaoLayerApplication.java
@@ -0,0 +1,13 @@
+package com.zygotecorp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbdaoLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbdaoLayerApplication.class, args);
+	}
+
+}
